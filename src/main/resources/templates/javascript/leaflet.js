ce724ef31ebb345aa6904aa004178421b1fd3397
Anthony Ruiz <anthonyra95@hotmail.com>
2020-09-14 22:44:14 -0400

    display albers projection if USA

---
+++ src/main/resources/templates/javascript/leaflet.js
@@ -0,0 +1,8 @@
+var mymap = L.map('map',{ zoomControl: false }).setView([38.500000, -98.000000], 4);
+L.geoJSON(albers_composite).addTo(mymap);
+
+mymap.dragging.disable();
+mymap.touchZoom.disable();
+mymap.doubleClickZoom.disable();
+mymap.scrollWheelZoom.disable();
+
---
+++ src/test/java/com/anthonyra95/covid/CovidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anthonyra95.covid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
