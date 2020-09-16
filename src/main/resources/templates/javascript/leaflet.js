b34a7a8b1a8b416196bca3eb12bac8eab733d32e
Anthony Ruiz <anthonyra95@hotmail.com>
2020-09-15 20:31:00 -0400

    made changes so that the files are served correctly by Spring. Map is displayed

--- src/main/resources/templates/javascript/leaflet.js
+++
@@ -1,8 +0,0 @@
-var mymap = L.map('map',{ zoomControl: false }).setView([38.500000, -98.000000], 4);
-L.geoJSON(albers_composite).addTo(mymap);
-
-mymap.dragging.disable();
-mymap.touchZoom.disable();
-mymap.doubleClickZoom.disable();
-mymap.scrollWheelZoom.disable();
-
